af42c56020b82ac679897cdecf7c62d93f78efd2
diankaaaa21 <diankapachina31@gmail.com>
2025-01-29 12:33:50 +0100

    telegram bot

--- database.py
+++ database.py
@@ -1,51 +1,42 @@
 import os
 from file_log import configurate_logger
-
 import mysql.connector
 from dotenv import load_dotenv
 
 logger = configurate_logger()
 load_dotenv()
 
-try:
-    conn = mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
-    )
-    cursor = conn.cursor()
-    logger.info("Connected to MySQL database successfully")
-except mysql.connector.Error as error:
-    logger.critical("Failed to connect to MySQL database")
-
 
 def execute_query(query, params=None):
     try:
         if params is None:
             params = ()
-        cursor.execute(query, params)
-        return cursor.fetchall() if cursor.with_rows else None
+        with mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
+        ) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query, params)
+                conn.commit()
+                return cursor.fetchall() if cursor.with_rows else None
     except mysql.connector.Error as err:
-        logger.error(f"MySQL error {err}")
-    finally:
-        conn.is_connected()
-        conn.close()
-        cursor.close()
+        logger.error(f"MySQL error: {err}")
+        return None
 
 
 def save_language(user_id, language):
     query = """
     INSERT INTO users (user_id, language) VALUES (%s, %s) 
-    ON DUBLICATE KEY UPDATE language='%s'"""
-    execute_query(query, (user_id, language))
+    ON DUPLICATE KEY UPDATE language=%s"""
+    execute_query(query, (user_id, language, language))
 
 
 def get_statistics():
     query = """
     SELECT language, COUNT(*) as count
-    from telegram_users
+    FROM users
     GROUP BY language 
     ORDER BY count DESC"""
-
     return execute_query(query)
